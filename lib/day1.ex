ef339a3360edb5e1efca6fcf5607817dbbbcb868
Jacob Parsell <jakep36@me.com>
2021-12-01 21:08:15 -0700

    part b

--- lib/day1.ex
+++ lib/day1.ex
@@ -12,14 +12,33 @@ defmodule AOC.Day1 do
     #   263/
     s = File.read!(Path.expand("./input/day1_part_a.txt"))
     part_a_calc(s)
+    part_b_calc(s)
+  end
+
+  def increases(measurements) do
+    measurements
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [x, y] -> y > x end)
+  end
+
+  def map_of_integers(s) do
+    s
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.to_integer/1)
   end
 
   def part_a_calc(input_string) do
     input_string
-    |> String.split("\n")
-    |> Enum.map(&String.trim/1)
-    |> Enum.map(&String.to_integer/1)
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.count(fn [x, y] -> y > x end)
+    |> map_of_integers()
+    |> increases()
+  end
+
+  def part_b_calc(input_string) do
+    input_string
+    |> map_of_integers
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> increases()
   end
 end
